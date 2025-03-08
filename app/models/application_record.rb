31275147eaeaa96be00bd34c47ef3eb76d7c5d59
Razum209 <ivan-raz@yandex.ru>
2025-03-08 13:28:35 +0300

    TEMA14-OR04

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,7 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def inspect
+    attributes.map { |key, value| "#{key}: #{value.inspect}" }.join(", ")
+  end
 end
---
+++ app/models/test.rb
@@ -0,0 +1,9 @@
+class Test < ApplicationRecord
+  belongs_to :category
+
+  has_and_belongs_to_many :users
+  def self.titles_by_category(category_title)
+    joins(:category).where(categories: {title: category_title}).order('tests.title DESC').pluck('tests.title')
+  # joins(:category).where(categories: { title: category_title }).order(title: :desc).pluck(:title)- или так
+  end
+end
